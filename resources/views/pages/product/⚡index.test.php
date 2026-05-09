004826c5f3a4ba500f9aa8653e9284b6c99c0797
Shaw <eastwoodshaw@gmail.com>
2026-05-09 13:23:38 +0100

    chore: move pages::product.index test

--- resources/views/pages/product/⚡index.test.php
+++ resources/views/pages/product/⚡index.test.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Enums\Category;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
 use function Pest\Livewire\livewire;
 
 uses(RefreshDatabase::class);
@@ -12,6 +14,65 @@ beforeEach(function () {
 	actingAs(
 		$this->user = User::factory()->create()
 	);
+});
+
+it('is accessible', function () {
+	get(route('products.index'))
+		->assertOk()
+		->assertSee('All Products');
+});
+
+it('displays empty message when no products exist', function () {
+	livewire('pages::product.index')
+		->assertSee('No products found.');
+});
+
+it('displays products', function () {
+	Product::factory()
+		->sequence(
+			['name' => 'Milk'],
+			['name' => 'Bread'],
+		)
+		->count(2)
+		->create();
+
+	livewire('pages::product.index')
+		->assertSee('Milk')
+		->assertSee('Bread');
+});
+
+it('can edit products', function () {
+	$product = Product::factory()
+		->state(['name' => 'Milk'])
+		->create();
+
+	livewire('pages::product.index')
+		->assertSet('editingProduct', null)
+		->call('edit', $product->id)
+		->assertSet('editingProduct.id', $product->id);
+});
+
+it('can filter by category', function () {
+	Product::factory()
+		->sequence(
+			['name' => 'Milk', 'category' => Category::Dairy],
+			['name' => 'Bread', 'category' => Category::Bakery]
+		)
+		->count(2)
+		->create();
+
+	livewire('pages::product.index')
+		->assertSee('Milk')
+		->assertSee('Bread')
+		->set('category', Category::Dairy->value)
+		->assertSee('Milk')
+		->assertDontSee('Bread')
+		->set('category', Category::Bakery->value)
+		->assertDontSee('Milk')
+		->assertSee('Bread')
+		->set('category', '')
+		->assertSee('Milk')
+		->assertSee('Bread');
 });
 
 it('can delete product', function () {
@@ -24,6 +85,19 @@ it('can delete product', function () {
 		->assertDispatched('modal-close', name: 'confirm-product-deletion');
 
 	$this->assertDatabaseMissing('products', [
+		'id' => $product->id,
+	]);
+});
+
+it('can cancel product deletion', function () {
+	$product = Product::factory()->create();
+
+	livewire('pages::product.index')
+		->set('productToDelete', $product)
+		->call('cancelDelete')
+		->assertSet('productToDelete', null);
+
+	$this->assertDatabaseHas('products', [
 		'id' => $product->id,
 	]);
 });
